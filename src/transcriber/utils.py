106bfd8016b3f67999682173d0d538e265f460dc
ar_571 <aaron.reinacher@iu-study.org>
2025-08-08 20:45:31 +0200

    Add conversion from video to audio and tests for ffmpeg installation

--- src/transcriber/utils.py
+++ src/transcriber/utils.py
@@ -1,3 +1,28 @@
+import subprocess
+
+
 def extract_audio(video_path, audio_path):
-    # TODO ffmpeg-Aufruf einbinden
-    return
+    """Extract audio as .wav from video file, according to whispers needs
+    :param str video_path: full path to video file
+    :param str audio_path: full path to audio file"""
+    try:
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-i", str(video_path),  # Eingabedatei
+                "-vn",  # kein Video
+                "-acodec", "pcm_s16le",  # unkomprimiertes WAV
+                "-ar", "16000",  # 16 kHz Samplerate
+                "-ac", "1",  # Mono
+                str(audio_path)
+            ],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    except FileNotFoundError:
+        raise RuntimeError(
+            "Fehler: ffmpeg nicht gefunden. Bitte installieren"
+        )
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"Fehler beim Extrahieren von Audio: {e.stderr.decode()}")
